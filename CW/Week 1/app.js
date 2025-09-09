76d75bce70777ed4dc58853d6b2c6bd79fce6a78
Kunga N Gurung <kgurung@touro.edu>
2025-09-09 19:10:19 -0400

    class assignments

---
+++ CW/Week 1/app.js
@@ -0,0 +1,44 @@
+'use strict';
+//const unchanged = "unchanged";
+let unchanged = "unchanged";
+unchanged = "change";
+console.log(unchanged);
+
+
+
+
+let num = 1;
+num= "Ram";
+console.log(num);
+
+
+const john = {name:"John" , age:25};
+john.name = "Jane";
+console.log(john);
+
+
+
+//creating an array
+let myarray = [1,2,3,"Four"];
+console.log(myarray);
+
+
+
+let students = [{name:"John" , age:25},{name:"Jane" , age:20},"Kunga","Ritik"];
+console.log(students);
+
+//variable is undefined for future purposes
+let student1;
+console.log(student1);
+
+
+student1 = {name:"Kunga" , age:25};
+console.log(student1);
+
+
+//cheking the data type
+let isCheck = true;
+console.log(typeof isCheck);
+
+let xyz=null;
+console.log(typeof xyz);
---
+++ CW/Week-2/function.js
@@ -0,0 +1,14 @@
+function fullname(first,last){
+    const fullname=first + " "+ last
+    return fullname
+}
+//Calling the function with the given arguments
+console.log(fullname('Kunga','Gurung'))
+
+
+
+function add(a,b){
+    const add=a + b
+    return add
+}
+console.log(add(5,5))
